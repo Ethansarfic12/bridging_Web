125620d9590da3a09b598526be4bacadefba0fa2
Ethansarfic12 <phyozawaung005@gmail.com>
2025-03-25 14:17:32 +0630

    updated

--- week1/tuple/park.py
+++ week1/tuple/park.py
@@ -1,7 +1,11 @@
 age = int (input ("Enter your age >> "))
 if age < 4 :
-    print("Your admission cost is 0.")
+    #print("Your admission cost is 0.")
+    price=0
 elif age <18:
-    print("Your admission cost is $25.")
+    #print("Your admission cost is $25.")
+    price = 25
 else:
-    print("Your admission cost is $50.")
+    #print("Your admission cost is $50.")
+    price = 40
+print(f"Your admission cost is $50.")
